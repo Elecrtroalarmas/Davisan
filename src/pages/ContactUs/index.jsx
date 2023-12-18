e30ff7392a35521ee5bb3cb6fe613e73dd02c631
David Fernando Pérez Medina <davidfernando1112@gmail.com>
2023-12-18 16:53:21 -0500

    Featrue: inicio contactus

--- src/pages/ContactUs/index.jsx
+++ src/pages/ContactUs/index.jsx
@@ -10,21 +10,70 @@ const ContactUs = () => {
       <Header />
       <main>
         <section>
-          <div className="contact-container">
-            <div className="container">
-                <div className="contact-box">
-                  <h2>Contactanos</h2>
-                  <p>Información de contacto</p>
-                </div>
-                  <div className="contact-box">
-                    <h2>Información</h2>
-                    <p>Email: contacto@example.com</p>
-                    <p>Teléfono: (123) 456-7890</p>
-                    <p>Dirección: Calle Ejemplo, Ciudad</p>
-                  </div>
+          <div className="contactUs">
+            <div className="title">
+              <h2>Contactanos</h2>
             </div>
-            <div className="map">
-              <img src="https://via.placeholder.com/1000x500" alt="Mapa"></img>
+            <div className="box">
+
+              {/* form box */}
+              <div className="contact form">
+                <h3>Envia un mensaje</h3>
+                <form>
+                  <div className="formBox">
+                    <div className="row50">
+                      <div className="inputBox">
+                        <span>Nombre</span>
+                        <input type="text" placeholder="Juan"/>
+                      </div>
+                      <div className="inputBox">
+                        <span>Apellido</span>
+                        <input type="text" placeholder="Maldonado"/>
+                      </div>
+                    </div>
+
+                    <div className="row50">
+                      <div className="inputBox">
+                        <span>Email</span>
+                        <input type="mail" placeholder="juan123@gmail.com"/>
+                      </div>
+                      <div className="inputBox">
+                        <span>Telefono</span>
+                        <input type="number" placeholder="318 938 9874"/>
+                      </div>
+                    </div>
+
+                    <div className="row100">
+                      <div className="inputBox">
+                        <span>Mensaje</span>
+                        <textarea placeholder="Escribe tu mensaje aqui..."></textarea>
+                      </div>
+                    </div>
+
+                    <div className="row100">
+                      <div className="inputBox">
+                        <input type="submit" placeholder="Enviar"/>
+                      </div>
+                    </div>
+
+
+                  </div>
+                </form>
+              </div>
+
+              {/* info box */}
+              <div className="contact info">
+                <h3>Informacion de contacto</h3>
+                <div className="infoBox">
+                  <div>
+                    <span></span>
+                    <p>Sogamoso, Boyaca <br/>Colombia</p>
+                  </div>
+                </div>
+              </div>
+
+              {/* map box */}
+              <div className="contact map"></div>
             </div>
           </div>
         </section>
