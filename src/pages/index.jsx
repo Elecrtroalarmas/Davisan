4432b11215ef0821429fc82f18b3195176c3e68e
Luis Alejandro Vera Moreno <98617444+SouthThunder@users.noreply.github.com>
2024-02-23 11:23:27 -0500

    update: home texts

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import Header from "../components/header/header";
 import Footer from "../components/footer/footer";
 import Button from "../components/buttons/button";
 import { CardSwipe } from "../components/card-swiper/card-swiper";
-import { Link, navigate } from "gatsby";
+import { Link } from "gatsby";
 import { RiBox2Line } from "react-icons/ri";
 import "./root.css";
 import "./index.css";
@@ -15,105 +15,98 @@ const IndexPage = () => {
     <>
       <Header />
       <main>
-
-
         <section className="main-banner">
           <div className="promt">
             <h1>Tu seguridad nuestra prioridad</h1>
             <p>
-              Our solutions are designed to help you to protect your business,
-              your customers, and your employees.
+              DAVISAN SECURITY LTDA, empresa de Seguridad y Vigilancia Privada
+              netamente colombiana, con visión de servicios para toda la
+              Sociedad.
             </p>
             <div className="options">
-              <Link to="/aboutUs"><Button text="Conocenos" /></Link>
-              <Link to="/ContactUs" ><Button text="Contactanos"/></Link>           
+              <Link to="/aboutUs">
+                <Button text="Conocenos" />
+              </Link>
+              <Link to="/ContactUs">
+                <Button text="Contactanos" />
+              </Link>
             </div>
           </div>
         </section>
 
-
-
-
-
         <hr />
 
-
         <section className="valores-section">
-            <div className='valorescon' id='valorescon'>
-              <div className="containerValores">
-                <h1 className='subTitulo'>Por qué elegirnos </h1>
-                <div className="valoresList">
-                  <div>
-                    <picture>
-                      <source srcSet="/calificacion.png" />
-                      <img src="/calificacion.png" alt="personal calificado" />
-                    </picture>
-                    <h2>Personal calificado</h2>
-                  </div>
-                  <div>
-                    <picture>
-                      <source srcSet="/consultoria.png" />
-                      <img src="/consultoria.png" alt="Consultoria" />
-                    </picture>
-                    <h2>Expertos en consultoría</h2>
-                  </div>
-                  <div>
-                    <picture>
-                      <source srcSet="/integracion.png" />
-                      <img src="/integracion.png" alt="integracion tecnologica" />
-                    </picture>
-                    <h2>Integración tecnológica</h2>
-                  </div>
-                  
-                  
+          <div className="valorescon" id="valorescon">
+            <div className="containerValores">
+              <h1 className="subTitulo">Por qué elegirnos </h1>
+              <div className="valoresList">
+                <div>
+                  <picture>
+                    <source srcSet="/calificacion.png" />
+                    <img src="/calificacion.png" alt="personal calificado" />
+                  </picture>
+                  <h2>Personal calificado</h2>
+                </div>
+                <div>
+                  <picture>
+                    <source srcSet="/consultoria.png" />
+                    <img src="/consultoria.png" alt="Consultoria" />
+                  </picture>
+                  <h2>Expertos en consultoría</h2>
+                </div>
+                <div>
+                  <picture>
+                    <source srcSet="/integracion.png" />
+                    <img src="/integracion.png" alt="integracion tecnologica" />
+                  </picture>
+                  <h2>Integración tecnológica</h2>
                 </div>
               </div>
             </div>
-          </section>
+          </div>
+        </section>
         <section className="services-section">
           <div className="promt">
             <div className="sub_promt">
-              <p>Protecting What Matters Most</p>
-              <h1>Advanced Surveillance Solutions</h1>
+              <p>Protegemos lo que verdaderamente importa</p>
+              <h1>Más de una Década de Excelencia protegiendo</h1>
               <p>
-                Our physical security products provide cutting-edge technology
-                and reliable solutions to keep your property, assets, and loved
-                ones safe. With our advanced surveillance systems, access
-                control systems, and intrusion detection systems, you can have
-                peace of mind knowing that your security is in good hands. We
-                also offer 24/7 monitoring services to ensure round-the-clock
-                protection.
+                Somos una empresa con una experiencia de 10 años en el mercado,
+                Comercial, Residencial. Con un grupo de clientes superior a los
+                329 abonados en Monitoreo de alarma para sus instalaciones.
               </p>
             </div>
             <div className="elements">
               <div className="item">
                 <RiBox2Line />
 
-                <h1>Access Control Systems</h1>
+                <h1>Vigilancia Humana Profesional</h1>
                 <p>
-                  Our advanced surveillance solutions utilize state-of-the-art
-                  cameras and analytics to provide real-time monitoring and
-                  proactive threat detection.
+                  Más de 25 clientes confían en nuestro equipo de vigilancia
+                  humana para proteger sus espacios comerciales y residenciales
+                  con profesionalismo y atención personalizada.
                 </p>
               </div>
               <div className="item">
                 <RiBox2Line />
 
-                <h1>Intrusion Detection Systems</h1>
+                <h1>Monitoreo Avanzado de Alarmas</h1>
                 <p>
-                  Our access control systems allow you to restrict access to
-                  authorized personnel only, enhancing security and preventing
-                  unauthorized entry.
+                  Más de 329 clientes confían en nuestro servicio de monitoreo
+                  de alarmas para proteger sus propiedades. Con 10 años de
+                  experiencia, prometemos una respuesta rápida y efectiva las 24
+                  horas del día.
                 </p>
               </div>
               <div className="item">
                 <RiBox2Line />
 
-                <h1>24/7 Monitoring Services</h1>
+                <h1>Servicios de Consultoría en Seguridad Integral</h1>
                 <p>
-                  Our intrusion detection systems use advanced sensors and
-                  alarms to detect and deter intruders, providing an additional
-                  layer of security for your property.
+                  Nuestro equipo de seguridad ofrece consultoría para negocios y
+                  hogares. Nos enfocamos en tus necesidades para proteger tus
+                  activos más importantes.
                 </p>
               </div>
             </div>
@@ -129,7 +122,7 @@ const IndexPage = () => {
         </section>
         <section className="testimonial-section">
           <div className="promt">
-              <CardSwipe />
+            <CardSwipe />
           </div>
         </section>
       </main>
